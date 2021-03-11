8ce7d1f66e44a6f290a5b9505a0e754b46ed2dd5
tuttibalutti <emmajbartl@gmail.com>
2021-03-11 23:58:06 +0100

    added hover effects, fillcolor in svg files, made page responsive

---
+++ code/src/Album.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Artist } from './Artist'
+
+const Album = (props) => {  //for albumArt: style={{ backgroundImage: `url(${props.image})` }}
+    return (
+        <div className="album">
+            <div className="albumArt" >
+                <img src={props.image} className="cover"></img>
+                <div className="overlay"></div>
+                <div className="icons">
+                    <img src="icons/heart.svg" className="heart"></img>
+                    <img src="icons/play.svg" className="play"></img>
+                    <img src="icons/dots.svg" className="dots"></img>
+                </div>
+            </div>
+            
+            
+
+            <div className="albumName">
+                <a href={props.albumUrl}>{props.albumName}</a>
+            </div>
+            <div className="artists">
+                {props.artists.map((artist, index) => {
+                if(index === props.artists.length-1) { //would mean that you are at the last artist
+                    console.log(index)
+                    console.log(artist.external_urls.spotify)
+                    return (<a href={artist.external_urls.spotify}>{artist.name}</a>)
+                }
+                else {  //would mean that you are NOT at the last artist
+                    return (<a href={artist.external_urls.spotify}>{artist.name}, </a>)
+                }
+                })}
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Album;
--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import data from './data.json'
+import Album from './Album'
+import Artist from './Artist'
 
-console.log(data)
+//TO-DO:
+//1. [_] get images to work - album cover image
+//2. [/] get urls to work - album & artist
+//2.5 [X] get artist names to become URLs to each artists own page
+//2.7 [X] get each artist name to link to their respective url, not just the first
+//3. [_] make Artist its own compenent which is called
+//4. [_] responsive - albums per row: 1 (mobile), 2 (tablet), 4 (desktop)
+//5. [_] hover effects
+
+console.log("album.artists.name:")
+console.log(data.albums.items[0])
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="app">
+      {data.albums.items.map((album) => {
+        return <Album albumName={album.name} artists={album.artists} image={album.images[0].url} albumUrl={album.external_urls.spotify} key={album.id}/>
+      })
+      }
+      {/* {data.albums.items.artists.map((artist) => {
+        return <Artist artistName={artist.name} key={artist.id}/>
+      })
+      } */}
     </div>
+    //.maps happen here we think
   )
 }
+
+

ae366971d359bd9e6db7f525f0b96f738ff89ffc
tuttibalutti <emmajbartl@gmail.com>
2021-03-12 14:49:13 +0100

    added to-do-list

--- code/src/Album.js
+++ code/src/Album.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Artist } from './Artist'
+//import { Artist } from './Artist'
 
 const Album = (props) => {  //for albumArt: style={{ backgroundImage: `url(${props.image})` }}
     return (
--- code/src/App.js
+++ code/src/App.js
@@ -4,13 +4,22 @@ import Album from './Album'
 import Artist from './Artist'
 
 //TO-DO:
-//1. [_] get images to work - album cover image
-//2. [/] get urls to work - album & artist
+//1. [X] get images to work - album cover image
+//2. [X] get urls to work - album & artist
 //2.5 [X] get artist names to become URLs to each artists own page
 //2.7 [X] get each artist name to link to their respective url, not just the first
 //3. [_] make Artist its own compenent which is called
-//4. [_] responsive - albums per row: 1 (mobile), 2 (tablet), 4 (desktop)
-//5. [_] hover effects
+//4. [X] responsive - albums per row: 1 (mobile), 2 (tablet), 4 (desktop)
+//5. [X] hover effects
+//10. [_] fix correct fonts
+//6. [_] readMe
+//7. [_] crossbrowser test
+//8. [_] netlify deploy
+//9. [_] restructure: AlbumWrapper which calls on components AlbumArt and Artist
+//11. [_] change names, Album.js, AlbumArt.js etc...
+
+
+
 
 console.log("album.artists.name:")
 console.log(data.albums.items[0])
@@ -19,7 +28,13 @@ export const App = () => {
   return (
     <div className="app">
       {data.albums.items.map((album) => {
-        return <Album albumName={album.name} artists={album.artists} image={album.images[0].url} albumUrl={album.external_urls.spotify} key={album.id}/>
+        return <Album 
+          albumName={album.name} 
+          artists={album.artists} 
+          image={album.images[0].url} 
+          albumUrl={album.external_urls.spotify} 
+          key={album.id}
+        />
       })
       }
       {/* {data.albums.items.artists.map((artist) => {
